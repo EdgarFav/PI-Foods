04ed3f065391e1f8ede276c00ad95af7982fc879
EdgarFav <edgarfda17@gmail.com>
2024-10-31 01:46:42 -0600

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const cors = require('cors')
 const routes = require('./routes/index.js');
 
 require('./db.js');
