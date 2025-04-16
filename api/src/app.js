7955bec941814e7623dc7517438ec71c00079fb9
EdgarFav <edgarfda17@gmail.com>
2025-04-16 14:10:44 -0600

    Fixing deploy

--- api/src/app.js
+++ api/src/app.js
@@ -15,14 +15,14 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors());
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://pi-foods-chi.vercel.app'); // update to match the domain you will make the request from
-//   // res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use((req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', 'https://pi-foods-chi.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/', routes);
 
